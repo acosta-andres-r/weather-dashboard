fafb03b758751fa105b1672f265fbfcffd81e8ae
Andres A <acosta.andres.r@gmail.com>
2020-08-14 13:34:04 -0500

    Obtained UV Index from its own endpoint, created and appended weather icon to body, and got object response for forcast API.

--- script.js
+++ script.js
@@ -1,28 +1,66 @@
 
 var city = "Houston";
 
-var api = "296147d25ca258f21c96f7c663b43bf3";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" 
-                + "units=imperial"
-                + "&appid=" + api
-                + "&q=" + city;
+// variable of API parts for easy access throughout the code
+var baseAPI = "https://api.openweathermap.org/data/2.5/"
+var endPointWeather = "weather" + "?";
+var endPointForecast = "forecast" + "?";
+var api = "appid=" + "296147d25ca258f21c96f7c663b43bf3";
+var unitsAPI = "&units=imperial";
+var query = "&q=" + city;
 
-console.log(queryURL);
+var queryURLweather = baseAPI + endPointWeather + api + unitsAPI + query;
+var queryURLforecast = baseAPI + endPointForecast + api + unitsAPI + query;
 
+var iconBaseURL = "http://openweathermap.org/img/wn/";
+
+// REQUEST: Current Weather
 $.ajax({
-    url: queryURL,
+    url: queryURLweather,
     method: "GET"
 })
-    .then(function (response) {
-        console.log(response);
+    .then(function (responseW) {
+        console.log(responseW);
+
+        var tempF = responseW.main.temp.toFixed(2) + " °F";
+        var humidity = responseW.main.humidity + " %";
+        var wind = responseW.wind.speed.toFixed(2) + " MPH";
+        // Get coordinates to pass them for UV endpoint 
+        var queryLat = "&lat=" + responseW.coord.lat;
+        var queryLon = "&lon=" + responseW.coord.lon;
+        var endPointUVI = "uvi" + "?";
+        queryURLuvi = baseAPI + endPointUVI + api + queryLat + queryLon;
+
+        // REQUEST: UVI by using coordinates of current weather request
+        $.ajax({
+            url: queryURLuvi,
+            method: "GET"
+        }).then(function (responseUVI) {
+
+            console.log(responseUVI);
+            var uvi = responseUVI.value.toFixed(2);
+            console.log("UV Index: " + uvi);
+        })
+
+        console.log("Temperature (F): " + tempF);
+        console.log("Humidity: " + humidity);
+        console.log("Wind Speed: " + wind);
         
-        var tempF = response.main.temp.toFixed(2) + " °F";
-        var humidity = response.main.humidity + " %";
-        var wind = response.wind.speed + " MPH";
-        
-        console.log("Temperature (F): " + tempF)
-        console.log("Humidity: " + humidity)
-        console.log("Wind Speed: " + wind)
+        // Get icon code to attach URL to a new image tag, then append to Body
+        var iconCode = responseW.weather[0].icon + "@2x.png";
+        var iconURL = iconBaseURL + iconCode;
+        console.log("icon URL: " + iconURL);
+        $("body").append($("<img>").attr("src", iconURL));
+
+    })
+
+// REQUEST: Forecast
+$.ajax({
+    url: queryURLforecast,
+    method: "GET"
+})
+    .then(function (responseF) {
+        console.log(responseF);
 
 
     })
