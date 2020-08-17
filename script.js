4f9f7a4bdd2416072a6c41a8844f7adcf0a2c5dd
Andres A <acosta.andres.r@gmail.com>
2020-08-16 21:13:59 -0500

    Got forecast data for its own endpoint.

--- script.js
+++ script.js
@@ -4,63 +4,90 @@ var city = "Houston";
 // variable of API parts for easy access throughout the code
 var baseAPI = "https://api.openweathermap.org/data/2.5/"
 var endPointWeather = "weather" + "?";
-var endPointForecast = "forecast" + "?";
 var api = "appid=" + "296147d25ca258f21c96f7c663b43bf3";
 var unitsAPI = "&units=imperial";
 var query = "&q=" + city;
 
 var queryURLweather = baseAPI + endPointWeather + api + unitsAPI + query;
-var queryURLforecast = baseAPI + endPointForecast + api + unitsAPI + query;
 
 var iconBaseURL = "http://openweathermap.org/img/wn/";
 
 // REQUEST: Current Weather
 $.ajax({
-    url: queryURLweather,
-    method: "GET"
+  url: queryURLweather,
+  method: "GET"
 })
-    .then(function (responseW) {
-        console.log(responseW);
+  .then(function (responseW) {
+    // console.log(responseW);
 
-        var tempF = responseW.main.temp.toFixed(2) + " °F";
-        var humidity = responseW.main.humidity + " %";
-        var wind = responseW.wind.speed.toFixed(2) + " MPH";
-        // Get coordinates to pass them for UV endpoint 
-        var queryLat = "&lat=" + responseW.coord.lat;
-        var queryLon = "&lon=" + responseW.coord.lon;
-        var endPointUVI = "uvi" + "?";
-        queryURLuvi = baseAPI + endPointUVI + api + queryLat + queryLon;
+    var currentTempF = responseW.main.temp.toFixed(2) + " °F";
+    var currentHumidity = responseW.main.humidity + " %";
+    var currentWind = responseW.wind.speed.toFixed(2) + " MPH";
+    var currentUVI = ""; // Used inside "Date of Forecast"
 
-        // REQUEST: UVI by using coordinates of current weather request
-        $.ajax({
-            url: queryURLuvi,
-            method: "GET"
-        }).then(function (responseUVI) {
+    // Get coordinates to pass them for Onecall endpoint 
+    var queryLat = "&lat=" + responseW.coord.lat;
+    var queryLon = "&lon=" + responseW.coord.lon;
+    var endPointOnecall = "onecall" + "?";
+    var excludeQuery = "&exclude=hourly,minutely"
 
-            console.log(responseUVI);
-            var uvi = responseUVI.value.toFixed(2);
-            console.log("UV Index: " + uvi);
-        })
+    var queryURLonecall = baseAPI + endPointOnecall + api + unitsAPI
+      + queryLat + queryLon + excludeQuery;
 
-        console.log("Temperature (F): " + tempF);
-        console.log("Humidity: " + humidity);
-        console.log("Wind Speed: " + wind);
-        
-        // Get icon code to attach URL to a new image tag, then append to Body
-        var iconCode = responseW.weather[0].icon + "@2x.png";
-        var iconURL = iconBaseURL + iconCode;
-        console.log("icon URL: " + iconURL);
-        $("body").append($("<img>").attr("src", iconURL));
+    // REQUEST: Onecall by using coordinates of current weather request
+    $.ajax({
+      url: queryURLonecall,
+      method: "GET"
+    }).then(function (responseOnecall) {
+
+      console.log(responseOnecall);
+
+      // DATE OF FORECAST
+      var i = 1 // Get first UV Index to assign to current UVI
+      var forecastDays = responseOnecall.daily;
+      for (forecastDay of forecastDays) {
+
+        // Extract Date of Forecast
+        var dateStr = new Date(forecastDay.dt * 1000).toString()
+        var [weekDay, month, day, year] = dateStr.split(" ")
+        // console.log(weekDay)
+
+        // Get temperature, humidity, wind speed, UV index
+        var TempF = forecastDay.temp.day;
+        var humidity = forecastDay.humidity;
+        var wind = forecastDay.wind_speed;
+
+        // UV Index
+        if (i === 1) {
+          currentUVI = forecastDay.uvi;
+          i++;
+          console.log("Current UV Index: " + currentUVI);
+        } else {
+          var uvi = forecastDay.uvi;
+          console.log("UV Index: " + uvi); // Sent current UV Index to a global variable
+        }
+
+        console.log("Temperature F: " + TempF + " °F");
+        console.log("Humidity: " + humidity + " %");
+        console.log("Wind Speed: " + wind + " MPH");
+        // Icon
+        var forecastIcon = forecastDay.weather[0].icon + "@2x.png";
+        var forecastIconURL = iconBaseURL + forecastIcon;
+        console.log("icon URL: " + forecastIconURL);
+        $("body").append($("<img>").attr("src", forecastIconURL));
+      }
 
     })
 
-// REQUEST: Forecast
-$.ajax({
-    url: queryURLforecast,
-    method: "GET"
-})
-    .then(function (responseF) {
-        console.log(responseF);
+    // CURRENT Weather
+    console.log("Current Temperature (F): " + currentTempF);
+    console.log("Current Humidity: " + currentHumidity);
+    console.log("Current Wind Speed: " + currentWind);
 
+    // Get icon code for URL to attach it into a new image tag, then append to Body
+    var iconCode = responseW.weather[0].icon + "@2x.png";
+    var iconURL = iconBaseURL + iconCode;
+    console.log("icon URL: " + iconURL);
+    $("body").append($("<img>").attr("src", iconURL));
 
-    })
+  })
